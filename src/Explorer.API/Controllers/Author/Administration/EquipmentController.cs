ab55a5c81d4569dd5462558d4db026cbfc3a0967
nina <ninabu16@gmail.com>
2024-03-14 02:12:33 +0100

    feat: get available equipment

--- src/Explorer.API/Controllers/Author/Administration/EquipmentController.cs
+++ src/Explorer.API/Controllers/Author/Administration/EquipmentController.cs
@@ -1,26 +1,49 @@
 ﻿using Explorer.Tours.API.Dtos;
-using Explorer.Tours.API.Public.Administration;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace Explorer.API.Controllers.Author.Administration
 {
     [Authorize(Policy = "authorPolicy")]
-    [Route("api/manipulation/equipment")]
+    [Route("api/tours")]
     public class EquipmentController : BaseApiController
     {
-        private readonly IEquipmentService _equipmentService;
+        private readonly HttpClient _httpClient;
 
-        public EquipmentController(IEquipmentService equipmentService)
+        public EquipmentController(IHttpClientFactory httpClientFactory)
         {
-            _equipmentService = equipmentService;
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri($"http://localhost:8081/tours");
         }
 
-        [HttpPost("get-available/{tourId:int}")]
-        public ActionResult<List<EquipmentDto>> GetAvailableEquipment([FromBody]List<long> currentEquipmentIds, int tourId)
+        [HttpGet("{id:long}/equipment/available")]
+        public async Task<ActionResult<List<EquipmentDto>>> GetAvailableEquipment(long id, [FromQuery] List<long>? equipmentIds)
         {
-            var result = _equipmentService.GetAvailable(currentEquipmentIds, tourId);
-            return CreateResponse(result);
+            var requestUri = ConstructUrl($"{id}/equipment/available", equipmentIds);
+
+            using var response = await _httpClient.GetAsync(requestUri);
+            var result = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return BadRequest($"Failed to get equipment: {result}");
+            }
+
+            var equipment = JsonSerializer.Deserialize<List<EquipmentDto>>(result);
+            return Ok(equipment);
+        }
+
+        private string ConstructUrl(string relativePath, List<long>? equipmentIds)
+        {
+            var requestUri = $"{_httpClient.BaseAddress}/{relativePath}";
+
+            if (equipmentIds is not { Count: > 0 }) return requestUri;
+
+            var queryParams = string.Join('&', equipmentIds.Select(id => $"equipmentIds={id}"));
+            requestUri += $"?{queryParams}";
+
+            return requestUri;
         }
     }
 }

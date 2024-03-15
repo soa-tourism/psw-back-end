2833cfe32bb803f5c2f266f64860cf4920f47f81
Dušan <duci.sudjic@gmail.com>
2024-03-15 15:16:48 +0100

    feat: encounter controller finished, authorization missing

--- src/Explorer.API/Controllers/Author/Administration/EncounterController.cs
+++ src/Explorer.API/Controllers/Author/Administration/EncounterController.cs
@@ -2,6 +2,7 @@
 using Explorer.BuildingBlocks.Core.UseCases;
 using Explorer.Encounters.API.Dtos;
 using Explorer.Encounters.API.Public;
+using Explorer.Encounters.Core.Domain.Encounters;
 using Explorer.Stakeholders.Infrastructure.Authentication;
 using Explorer.Tours.API.Dtos;
 using Explorer.Tours.API.Public.Administration;
@@ -10,6 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
 using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Explorer.API.Controllers.Author.Administration
 {
@@ -64,34 +66,68 @@ namespace Explorer.API.Controllers.Author.Administration
             return CreateResponse(new Result<EncounterDto>().WithError("Error"));
         }
 
-        //[HttpPut]
-        //public ActionResult<EncounterDto> Update([FromForm] EncounterDto encounter, [FromForm] List<IFormFile>? imageF = null)
-        //{
+        [HttpPut]
+        public async Task<ActionResult<EncounterDto>> Update([FromForm] EncounterDto encounter, [FromForm] List<IFormFile>? imageF = null)
+        {
 
-        //    if (imageF != null && imageF.Any())
-        //    {
-        //        var imageNames = _imageService.UploadImages(imageF);
-        //        if (encounter.Type == "Location")
-        //            encounter.Image = imageNames[0];
-        //    }
+            if (imageF != null && imageF.Any())
+            {
+                var imageNames = _imageService.UploadImages(imageF);
+                if (encounter.Type == "Location")
+                    encounter.Image = imageNames[0];
+            }
+            var json = JsonSerializer.Serialize(encounter);
 
-        //    var result = _encounterService.Update(encounter,User.PersonId());
-        //    return CreateResponse(result);
-        //}
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-        //[HttpDelete("{id:int}")]
-        //public ActionResult Delete(int id)
-        //{
-        //    var result = _encounterService.Delete(id, User.PersonId());
-        //    return CreateResponse(result);
-        //}
+            HttpResponseMessage response = await client.PutAsync($"{baseUrl}/update", content);
+            Response.ContentType = "application/json";
+            if (response.IsSuccessStatusCode)
+            {
+                // Read response content as JSON string
+                string responseJson = await response.Content.ReadAsStringAsync();
 
-        //[HttpGet("{id:int}")]
-        //public ActionResult<EncounterDto> GetById(int id)
-        //{
-        //    var result = _encounterService.Get(id);
-        //    return CreateResponse(result);
-        //}
+                // Deserialize JSON string to EncounterDto object
+                EncounterDto responseEncounter = JsonSerializer.Deserialize<EncounterDto>(responseJson);
+
+                var result = new Result<EncounterDto>().WithValue(responseEncounter);
+                // Use the responseEncounter object as needed
+                return CreateResponse(result);
+            }
+
+
+            return CreateResponse(new Result<EncounterDto>().WithError("Error"));
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync($"{baseUrl}/delete/{id}");
+            return StatusCode((int)response.StatusCode);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<EncounterDto>> GetById(int id)
+        {
+
+            HttpResponseMessage response = await client.GetAsync($"{baseUrl}/get/{id}");
+            Response.ContentType = "application/json";
+            if (response.IsSuccessStatusCode)
+            {
+                // Read response content as JSON string
+                string responseJson = await response.Content.ReadAsStringAsync();
+
+                // Deserialize JSON string to EncounterDto object
+                EncounterDto responseEncounter = JsonSerializer.Deserialize<EncounterDto>(responseJson);
+
+                var result = new Result<EncounterDto>().WithValue(responseEncounter);
+                // Use the responseEncounter object as needed
+                return CreateResponse(result);
+            }
+
+
+            return CreateResponse(new Result<EncounterDto>().WithError("Error"));
+        }
 
     }
 }

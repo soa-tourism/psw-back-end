70752f897d07ccb47c790db59ac79647247b422e
nina <ninabu16@gmail.com>
2024-04-25 09:45:01 +0200

    feat: add some removed controllers

---
+++ src/Explorer.API/Controllers/Administrator/Administration/AccountsManagementController.cs
@@ -0,0 +1,35 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Administrator.Administration
+{
+    [Authorize(Policy = "administratorPolicy")]
+    [Route("api/administration/accountsManagement")]
+    public class AccountsManagementController : BaseApiController
+    {
+        private readonly IAccountsManagementService _accountsManagementService;
+
+        public AccountsManagementController(IAccountsManagementService accountsManagementService)
+        {
+            _accountsManagementService = accountsManagementService;
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<UserDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _accountsManagementService.GetPaged(page, pageSize);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("block/{id:int}")]
+        public ActionResult<UserDto> Block(int id)
+        {
+            var result = _accountsManagementService.Block(id);
+            return CreateResponse(result);
+        }
+
+    }
+}
---
+++ src/Explorer.API/Controllers/Administrator/Administration/ReviewGradeController.cs
@@ -0,0 +1,27 @@
+﻿using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Administrator.Administration
+{
+    [Authorize(Policy = "administratorPolicy")]
+    [Route("api/administration/applicationGrade")]
+    public class ReviewGradeController : BaseApiController
+    {
+        private readonly IApplicationGradeService _applicationGradeService;
+
+        public ReviewGradeController(IApplicationGradeService applicationGradeService)
+        {
+            _applicationGradeService = applicationGradeService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<ApplicationGradeDto>> ReviewGrades(int page, int pageSize)
+        {
+            var result = _applicationGradeService.ReviewGrades(page, pageSize);
+            return CreateResponse(result);
+        }
+
+    }
+}

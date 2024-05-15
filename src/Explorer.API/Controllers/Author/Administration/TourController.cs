b1db8b4df0d0c43576c3da646ec8fb13b6881bb3
jelly0107 <jelisaveta.letic@gmail.com>
2024-05-15 14:01:14 +0200

    feat: tours features

--- src/Explorer.API/Controllers/Author/Administration/TourController.cs
+++ src/Explorer.API/Controllers/Author/Administration/TourController.cs
@@ -21,149 +21,149 @@ namespace Explorer.API.Controllers.Author.Administration
         }
 
 
-        [HttpGet]
-        public async Task<ActionResult<PagedResult<BasicTourDto>>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
-        {
-            using var response = await _httpClient.GetAsync("");
-            var result = await response.Content.ReadAsStringAsync();
+        //[HttpGet]
+        //public async Task<ActionResult<PagedResult<BasicTourDto>>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        //{
+        //    using var response = await _httpClient.GetAsync("");
+        //    var result = await response.Content.ReadAsStringAsync();
 
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = Result.Fail($"Failed to get tours: {result}");
-                return CreateResponse(error);
-            }
+        //    if (!response.IsSuccessStatusCode)
+        //    {
+        //        var error = Result.Fail($"Failed to get tours: {result}");
+        //        return CreateResponse(error);
+        //    }
 
-            var tours = JsonSerializer.Deserialize<List<BasicTourDto>>(result);
-            if (tours is null)
-            {
-                var noTours = Result.Ok("No tours found.");
-                return CreateResponse(noTours);
+        //    var tours = JsonSerializer.Deserialize<List<BasicTourDto>>(result);
+        //    if (tours is null)
+        //    {
+        //        var noTours = Result.Ok("No tours found.");
+        //        return CreateResponse(noTours);
 
-            }
+        //    }
 
-            var pagedResult = PaginateResult(page, pageSize, tours);
-            return Ok(pagedResult);
-        }
+        //    var pagedResult = PaginateResult(page, pageSize, tours);
+        //    return Ok(pagedResult);
+        //}
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<BasicTourDto>> GetById(string id)
-        {
-            using var response = await _httpClient.GetAsync(ConstructUrl(id));
-            var result = await response.Content.ReadAsStringAsync();
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<BasicTourDto>> GetById(string id)
+        //{
+        //    using var response = await _httpClient.GetAsync(ConstructUrl(id));
+        //    var result = await response.Content.ReadAsStringAsync();
 
-            if (!response.IsSuccessStatusCode)
-            {
-                var errorResult = Result.Fail($"Failed to get tour: {result}");
-                return CreateResponse(errorResult);
-            }
+        //    if (!response.IsSuccessStatusCode)
+        //    {
+        //        var errorResult = Result.Fail($"Failed to get tour: {result}");
+        //        return CreateResponse(errorResult);
+        //    }
 
-            var tour = JsonSerializer.Deserialize<BasicTourDto>(result);
-            return Ok(tour);
-        }
+        //    var tour = JsonSerializer.Deserialize<BasicTourDto>(result);
+        //    return Ok(tour);
+        //}
 
-        [HttpPost]
-        public async Task<ActionResult<BasicTourDto>> Create([FromBody] BasicTourDto tour)
-        {
-            using var jsonContent = new StringContent(JsonSerializer.Serialize(tour), Encoding.UTF8, "application/json");
-            using var response = await _httpClient.PostAsync("", jsonContent);
+        //[HttpPost]
+        //public async Task<ActionResult<BasicTourDto>> Create([FromBody] BasicTourDto tour)
+        //{
+        //    using var jsonContent = new StringContent(JsonSerializer.Serialize(tour), Encoding.UTF8, "application/json");
+        //    using var response = await _httpClient.PostAsync("", jsonContent);
 
-            var responseContent = await response.Content.ReadAsStringAsync();
-            var result = JsonSerializer.Deserialize<BasicTourDto>(responseContent);
+        //    var responseContent = await response.Content.ReadAsStringAsync();
+        //    var result = JsonSerializer.Deserialize<BasicTourDto>(responseContent);
 
-            return CreateResponse(result.ToResult());
-        }
+        //    return CreateResponse(result.ToResult());
+        //}
 
-        [HttpPut("{id}")]
-        public async Task<ActionResult<BasicTourDto>> Update([FromBody] BasicTourDto tour, string id)
-        {
-            using var jsonContent = new StringContent(JsonSerializer.Serialize(tour), Encoding.UTF8, "application/json");
-            using var response = await _httpClient.PutAsync(ConstructUrl(id), jsonContent);
+        //[HttpPut("{id}")]
+        //public async Task<ActionResult<BasicTourDto>> Update([FromBody] BasicTourDto tour, string id)
+        //{
+        //    using var jsonContent = new StringContent(JsonSerializer.Serialize(tour), Encoding.UTF8, "application/json");
+        //    using var response = await _httpClient.PutAsync(ConstructUrl(id), jsonContent);
 
-            var responseContent = await response.Content.ReadAsStringAsync();
-            var result = JsonSerializer.Deserialize<BasicTourDto>(responseContent);
+        //    var responseContent = await response.Content.ReadAsStringAsync();
+        //    var result = JsonSerializer.Deserialize<BasicTourDto>(responseContent);
 
-            return CreateResponse(result.ToResult());
-        }
+        //    return CreateResponse(result.ToResult());
+        //}
 
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> Delete(string id)
-        {
-            using var response = await _httpClient.DeleteAsync(ConstructUrl(id.ToString()));
+        //[HttpDelete("{id}")]
+        //public async Task<ActionResult> Delete(string id)
+        //{
+        //    using var response = await _httpClient.DeleteAsync(ConstructUrl(id.ToString()));
 
-            if (response.IsSuccessStatusCode) return NoContent();
+        //    if (response.IsSuccessStatusCode) return NoContent();
 
-            var errorResult = Result.Fail("Failed to delete equipment.");
-            return CreateResponse(errorResult);
-        }
+        //    var errorResult = Result.Fail("Failed to delete equipment.");
+        //    return CreateResponse(errorResult);
+        //}
 
-        [HttpGet("author/{id:long}")]
-        public async Task<ActionResult<List<BasicTourDto>>> GetByAuthor([FromQuery] int page, [FromQuery] int pageSize, long id)
-        {
-            var requestUri = ConstructUrl($"author/{id}");
+        //[HttpGet("author/{id:long}")]
+        //public async Task<ActionResult<List<BasicTourDto>>> GetByAuthor([FromQuery] int page, [FromQuery] int pageSize, long id)
+        //{
+        //    var requestUri = ConstructUrl($"author/{id}");
 
-            using var response = await _httpClient.GetAsync(requestUri);
-            var result = await response.Content.ReadAsStringAsync();
+        //    using var response = await _httpClient.GetAsync(requestUri);
+        //    var result = await response.Content.ReadAsStringAsync();
 
-            if (!response.IsSuccessStatusCode)
-            {
-                return BadRequest($"Failed to get tour: {result}");
-            }
+        //    if (!response.IsSuccessStatusCode)
+        //    {
+        //        return BadRequest($"Failed to get tour: {result}");
+        //    }
 
-            var tours = JsonSerializer.Deserialize<List<BasicTourDto>>(result);
-            if (tours is null)
-            {
-                var noTours = Result.Ok("No tours found.");
-                return CreateResponse(noTours);
+        //    var tours = JsonSerializer.Deserialize<List<BasicTourDto>>(result);
+        //    if (tours is null)
+        //    {
+        //        var noTours = Result.Ok("No tours found.");
+        //        return CreateResponse(noTours);
 
-            }
+        //    }
 
-            var pagedResult = PaginateResult(page, pageSize, tours);
-            return Ok(pagedResult);
-        }
+        //    var pagedResult = PaginateResult(page, pageSize, tours);
+        //    return Ok(pagedResult);
+        //}
 
-        [HttpPost("{id}/equipment/{equipmentId}")]
-        public async Task<ActionResult> AddEquipment(string id, string equipmentId)
-        {
-            using var response = await _httpClient.PostAsync(ConstructUrl($"{id}/equipment/{equipmentId}"), null);
+        //[HttpPost("{id}/equipment/{equipmentId}")]
+        //public async Task<ActionResult> AddEquipment(string id, string equipmentId)
+        //{
+        //    using var response = await _httpClient.PostAsync(ConstructUrl($"{id}/equipment/{equipmentId}"), null);
 
-            if (response.IsSuccessStatusCode) return Ok();
+        //    if (response.IsSuccessStatusCode) return Ok();
 
-            var errorResult = Result.Fail("Failed to add equipment.");
-            return CreateResponse(errorResult);
-        }
+        //    var errorResult = Result.Fail("Failed to add equipment.");
+        //    return CreateResponse(errorResult);
+        //}
 
-        [HttpDelete("{id}/equipment/{equipmentId}")]
-        public async Task<ActionResult> RemoveEquipment(string id, string equipmentId)
-        {
-            using var response = await _httpClient.DeleteAsync(ConstructUrl($"{id}/equipment/{equipmentId}"));
+        //[HttpDelete("{id}/equipment/{equipmentId}")]
+        //public async Task<ActionResult> RemoveEquipment(string id, string equipmentId)
+        //{
+        //    using var response = await _httpClient.DeleteAsync(ConstructUrl($"{id}/equipment/{equipmentId}"));
 
-            if (response.IsSuccessStatusCode) return NoContent();
+        //    if (response.IsSuccessStatusCode) return NoContent();
 
-            var errorResult = Result.Fail("Failed to delete equipment.");
-            return CreateResponse(errorResult);
-        }
+        //    var errorResult = Result.Fail("Failed to delete equipment.");
+        //    return CreateResponse(errorResult);
+        //}
 
 
-        // [HttpPut("publishedTours/{id:int}")]
-        // public ActionResult<BasicTourDto> Publish(int id)
-        // {
-        //     var result = _tourService.Publish(id, User.PersonId());
-        //     return CreateResponse(result);
-        // }
-        //
-        // [HttpPut("archivedTours/{id:int}")]
-        // public ActionResult<BasicTourDto> Archive(int id)
-        // {
-        //     var result = _tourService.Archive(id, User.PersonId());
-        //     return CreateResponse(result);
-        // }
-        //
-        // [HttpPut("tourTime/{id:int}")]
-        // public ActionResult<BasicTourDto> AddTime(TourTimesDto tourTimesDto, int id)
-        // {
-        //     var result = _tourService.AddTime(tourTimesDto, id, User.PersonId());
-        //     return CreateResponse(result);
-        // }
+        //// [HttpPut("publishedTours/{id:int}")]
+        //// public ActionResult<BasicTourDto> Publish(int id)
+        //// {
+        ////     var result = _tourService.Publish(id, User.PersonId());
+        ////     return CreateResponse(result);
+        //// }
+        ////
+        //// [HttpPut("archivedTours/{id:int}")]
+        //// public ActionResult<BasicTourDto> Archive(int id)
+        //// {
+        ////     var result = _tourService.Archive(id, User.PersonId());
+        ////     return CreateResponse(result);
+        //// }
+        ////
+        //// [HttpPut("tourTime/{id:int}")]
+        //// public ActionResult<BasicTourDto> AddTime(TourTimesDto tourTimesDto, int id)
+        //// {
+        ////     var result = _tourService.AddTime(tourTimesDto, id, User.PersonId());
+        ////     return CreateResponse(result);
+        //// }
 
         private string ConstructUrl(string relativePath)
         {
--- src/Explorer.API/ProtoControllers/TourProtoController.cs
+++ src/Explorer.API/ProtoControllers/TourProtoController.cs
@@ -39,7 +39,7 @@ namespace Explorer.API.ProtoControllers
 
             return await Task.FromResult(response);
         }
-        public override async Task<EquipmentResponse> GetEquipment(EquipmentId id,
+        public override async Task<EquipmentResponse> GetEquipment(Id id,
             ServerCallContext context)
         {
             var httpHandler = new HttpClientHandler();
@@ -75,7 +75,7 @@ namespace Explorer.API.ProtoControllers
 
             return await Task.FromResult(response);
         }
-        public override async Task<EquipmentResponse> DeleteEquipment(EquipmentId id,
+        public override async Task<EquipmentResponse> DeleteEquipment(Id id,
             ServerCallContext context)
         {
             var httpHandler = new HttpClientHandler();
@@ -87,5 +87,110 @@ namespace Explorer.API.ProtoControllers
 
             return await Task.FromResult(response);
         }
+
+        // Tour
+        public override async Task<PagedToursResponse> GetAllTour(Page page,
+            ServerCallContext context)
+        {
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var channel = GrpcChannel.ForAddress("http://host.docker.internal:8083/v1/tours", new GrpcChannelOptions { HttpHandler = httpHandler });
+
+            var client = new Tour.TourClient(channel);
+            var response = client.GetAllTour(page);
+
+            return await Task.FromResult(response);
+        }
+        public override async Task<TourResponse> GetTour(Id id,
+            ServerCallContext context)
+        {
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var channel = GrpcChannel.ForAddress("http://host.docker.internal:8083/v1/tours", new GrpcChannelOptions { HttpHandler = httpHandler });
+
+            var client = new Tour.TourClient(channel);
+            var response = client.GetTour(id);
+
+            return await Task.FromResult(response);
+        }
+        public override async Task<TourResponse> CreateTour(TourResponse tour,
+            ServerCallContext context)
+        {
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var channel = GrpcChannel.ForAddress("http://host.docker.internal:8083/v1/tours", new GrpcChannelOptions { HttpHandler = httpHandler });
+
+            var client = new Tour.TourClient(channel);
+            var response = client.CreateTour(tour);
+
+            return await Task.FromResult(response);
+        }
+        public override async Task<TourResponse> UpdateTour(UpdateTourId tour,
+            ServerCallContext context)
+        {
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var channel = GrpcChannel.ForAddress("http://host.docker.internal:8083/v1/tours", new GrpcChannelOptions { HttpHandler = httpHandler });
+
+            var client = new Tour.TourClient(channel);
+            var response = client.UpdateTour(tour);
+
+            return await Task.FromResult(response);
+        }
+        public override async Task<blank> DeleteTour(Id id,
+            ServerCallContext context)
+        {
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var channel = GrpcChannel.ForAddress("http://host.docker.internal:8083/v1/tours", new GrpcChannelOptions { HttpHandler = httpHandler });
+
+            var client = new Tour.TourClient(channel);
+            var response = client.DeleteTour(id);
+
+            return await Task.FromResult(response);
+        }
+        public override async Task<ToursResponse> GetTourByAuthorId(TourByAuthorId id,
+            ServerCallContext context)
+        {
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var channel = GrpcChannel.ForAddress("http://host.docker.internal:8083/v1/tours", new GrpcChannelOptions { HttpHandler = httpHandler });
+
+            var client = new Tour.TourClient(channel);
+            var response = client.GetTourByAuthorId(id);
+
+            return await Task.FromResult(response);
+        }
+        public override async Task<blank> AddTourEquipment(TourEquipment equipment,
+            ServerCallContext context)
+        {
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var channel = GrpcChannel.ForAddress("http://host.docker.internal:8083/v1/tours", new GrpcChannelOptions { HttpHandler = httpHandler });
+
+            var client = new Tour.TourClient(channel);
+            var response = client.AddTourEquipment(equipment);
+
+            return await Task.FromResult(response);
+        }
+        public override async Task<blank> DeleteTourEquipment(TourEquipment equipment,
+            ServerCallContext context)
+        {
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var channel = GrpcChannel.ForAddress("http://host.docker.internal:8083/v1/tours", new GrpcChannelOptions { HttpHandler = httpHandler });
+
+            var client = new Tour.TourClient(channel);
+            var response = client.DeleteTourEquipment(equipment);
+
+            return await Task.FromResult(response);
+        }
+
+        // PublishedTour
+        // TourReview
+        // PublicCheckpoint
+        // Checkpoint
+        // TouristPosition
+        // TourExecution
     }
 }
